38e3fa4f456e7a2582444997a33f2bf8a39afc4a
adil.kenzhigalieyev <adil.kenzhigaliyev@git.01.alem.school>
2021-07-25 23:15:02 +0600

    here

--- functions/ztail/ztail.go
+++ functions/ztail/ztail.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"student/functions"
 )
 
 func main() {
-	arguments := os.Args[1:]
+	arguments := os.Args
+	// fmt.Println(len(arguments))
 	arg, number, arr := IsValid(arguments)
+	// fmt.Println(number, arr, arg)
 	if !arg {
 		os.Stdout.ReadFrom(os.Stdin)
 	} else {
 		for _, value := range arr {
-			info, err := ioutil.ReadFile(value)
+			info, err := os.Open(value)
 			if err != nil {
-				fmt.Println("ERROR:" + value)
+				fmt.Println("ztail:")
+				// os.Stdout.Write(err)
+
 			}
-			length := len(info)
-			fmt.Println(string(info)[length-number:])
+			defer info.Close()
+			data := make([]byte, 64)
+			for {
+				values, err := info.Read(data)
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("%s", string(data[values-number:values]))
+				// length := len(data)
+				// fmt.Println(string(data)[length-number : length])
+			}
+			fmt.Printf("==> %s <==", value)
 		}
 	}
+
+	// arguments := os.Args[1:]
+	// arg, number, arr := IsValid(arguments)
+	// if !arg {
+	// 	os.Stdout.ReadFrom(os.Stdin)
+	// } else {
+	// 	for _, value := range arr {
+	// 		info, err := ioutil.ReadFile(value)
+	// 		if err != nil {
+	// 			fmt.Println("ERROR:" + value)
+	// 		}
+	// 		length := len(info)
+	// 		fmt.Println(string(info)[length-number:])
+	// 	}
+	// }
 }
 
 func IsValid(arguments []string) (bool, int, []string) {
@@ -47,22 +76,5 @@ func IsValid(arguments []string) (bool, int, []string) {
 		}
 	}
 
-	// for index, val := range arguments {
-	// 	fmt.Println(index)
-	// 	if val == "-c" && index != len(arguments) {
-	// 		if arguments[index+1] == "0" {
-	// 			index = index + 2
-	// 			result = 0
-	// 		} else if functions.Atoi(arguments[index+1]) != 0 {
-	// 			result = functions.Atoi(arguments[index+1])
-	// 			index = index + 2
-	// 		} else {
-	// 			return false, result, str
-	// 		}
-	// 	} else {
-	// 		str = append(str, val)
-	// 	}
-	// }
-	// fmt.Println(result, str)
 	return true, result, str
 }
