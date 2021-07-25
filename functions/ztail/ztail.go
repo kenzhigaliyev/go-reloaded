50784a5b4c026eb56055ec203d7bc04cbeed2b63
adil.kenzhigalieyev <adil.kenzhigaliyev@git.01.alem.school>
2021-07-25 16:19:26 +0600

    here

--- functions/ztail/ztail.go
+++ functions/ztail/ztail.go
@@ -4,19 +4,65 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"student/functions"
 )
 
 func main() {
 	arguments := os.Args[1:]
-	if len(arguments) == 0 {
+	arg, number, arr := IsValid(arguments)
+	if !arg {
 		os.Stdout.ReadFrom(os.Stdin)
 	} else {
-		for _, value := range arguments {
+		for _, value := range arr {
 			info, err := ioutil.ReadFile(value)
 			if err != nil {
 				fmt.Println("ERROR:" + value)
 			}
-			fmt.Print(string(info))
+			length := len(info)
+			fmt.Println(string(info)[length-number:])
 		}
 	}
+}
+
+func IsValid(arguments []string) (bool, int, []string) {
+	var str []string
+	var result int
+	if len(arguments) == 0 {
+		return false, result, str
+	}
+	for index := 0; index < len(arguments); index++ {
+		// fmt.Println(index)
+		if arguments[index] == "-c" && index != len(arguments) {
+			if arguments[index+1] == "0" {
+				index = index + 1
+				result = 0
+			} else if functions.Atoi(arguments[index+1]) != 0 {
+				result = functions.Atoi(arguments[index+1])
+				index = index + 1
+			} else {
+				return false, result, str
+			}
+		} else {
+			str = append(str, arguments[index])
+		}
+	}
+
+	// for index, val := range arguments {
+	// 	fmt.Println(index)
+	// 	if val == "-c" && index != len(arguments) {
+	// 		if arguments[index+1] == "0" {
+	// 			index = index + 2
+	// 			result = 0
+	// 		} else if functions.Atoi(arguments[index+1]) != 0 {
+	// 			result = functions.Atoi(arguments[index+1])
+	// 			index = index + 2
+	// 		} else {
+	// 			return false, result, str
+	// 		}
+	// 	} else {
+	// 		str = append(str, val)
+	// 	}
+	// }
+	// fmt.Println(result, str)
+	return true, result, str
 }
