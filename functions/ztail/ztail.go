0c9175c01e2a8e509b1124ff4b6eb110e30cdb4f
adil.kenzhigalieyev <adil.kenzhigaliyev@git.01.alem.school>
2021-07-27 14:21:20 +0600

    here

--- functions/ztail/ztail.go
+++ functions/ztail/ztail.go
@@ -12,12 +12,14 @@ import (
 func main() {
 	arguments := os.Args[1:]
 	var new int
-	arg, number, arr := IsValid(arguments)
+	arg, number, arr, positive := IsValid(arguments)
+	positive = positive
 	if arg == false {
 		fmt.Printf("tail: option requires an argument -- 'c'\nTry 'tail --help' for more information.\n")
 		return
 	}
-
+	// fmt.Println(number)
+	number = number
 	for index, value := range arr {
 		file, err := os.Open(value)
 		if err != nil {
@@ -25,9 +27,14 @@ func main() {
 			z01.PrintRune('\n')
 			os.Exit(1)
 		}
+		info, err := file.Stat()
+		if err != nil {
+
+		}
+		length := info.Size()
 		defer file.Close()
 
-		data := make([]byte, 64)
+		data := make([]byte, length)
 
 		for {
 			n, err := file.Read(data)
@@ -39,76 +46,40 @@ func main() {
 		if len(arr) != 1 {
 			fmt.Printf("==> %s <==", value)
 			z01.PrintRune('\n')
-			fmt.Print(string(data[new-number : new]))
+			fmt.Print(data[:number])
 			if index != len(arr)-1 {
 				z01.PrintRune('\n')
 			}
 		} else {
-			fmt.Print(string(data[new-number : new]))
+			fmt.Print(string(data[:new]))
 		}
-
-		// for _, value := range arr {
-		// 	info, err := os.Open(value)
-		// 	if err != nil {
-		// 		defer os.Exit(1)
-		// 		fmt.Println("ztail:")
-		// 		// os.Stdout.Write(err)
-
-		// 	}
-		// 	defer info.Close()
-		// 	data := make([]byte, 64)
-		// 	for {
-		// 		values, err := info.Read(data)
-		// 		if err == io.EOF {
-		// 			break
-		// 		}
-		// 		fmt.Printf("%s", string(data[values-number:values]))
-		// 		// length := len(data)
-		// 		// fmt.Println(string(data)[length-number : length])
-		// 	}
-		// 	fmt.Printf("==> %s <==", value)
-		// }
 	}
-
-	// arguments := os.Args[1:]
-	// arg, number, arr := IsValid(arguments)
-	// if !arg {
-	// 	os.Stdout.ReadFrom(os.Stdin)
-	// } else {
-	// 	for _, value := range arr {
-	// 		info, err := ioutil.ReadFile(value)
-	// 		if err != nil {
-	// 			fmt.Println("ERROR:" + value)
-	// 		}
-	// 		length := len(info)
-	// 		fmt.Println(string(info)[length-number:])
-	// 	}
-	// }
 }
 
-func IsValid(arguments []string) (bool, int, []string) {
+func IsValid(arguments []string) (bool, uint64, []string, bool) {
 	var str []string
-	var result int
+	var result uint64
+	var positive bool
 	if len(arguments) == 0 {
-		return false, result, str
+		return false, result, str, positive
 	}
 	for index := 0; index < len(arguments); index++ {
 		if arguments[index] == "-c" && index != len(arguments)-1 {
+			// fmt.Println(arguments[index+1])
 			if arguments[index+1] == "0" {
 				index = index + 1
 				result = 0
-			} else if functions.Atoi(arguments[index+1]) != 0 {
-				result = functions.Atoi(arguments[index+1])
+			} else if len(arguments[index+1]) < len("18446744073709551615") {
+				result, positive = functions.AtoiUnix(arguments[index+1])
 				index = index + 1
 			} else {
-				return false, result, str
+				return false, result, str, positive
 			}
 		} else if arguments[index] == "-c" && index == len(arguments)-1 {
-			return false, result, str
+			return false, result, str, positive
 		} else {
 			str = append(str, arguments[index])
 		}
 	}
-
-	return true, result, str
+	return true, result, str, positive
 }
